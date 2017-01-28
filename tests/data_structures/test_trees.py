fa329bae851337cfec7ba77b9adb4a923c1d1f72
Nikhil Narang <nikhilnarang@Nikhils-MacBook.local>
2017-01-28 14:48:37 -0800

    merged heaps into trees

--- tests/data_structures/test_trees.py
+++ tests/data_structures/test_trees.py
@@ -1,6 +1,7 @@
 import os, sys, unittest, random
 sys.path.append(os.path.join(os.getcwd(), os.path.pardir))
 from data_structures.trees.binary_search_tree import BinarySearchTree
+from data_structures.trees.binary_heap import BinaryHeap
 
 class TestTreeDataStructures(unittest.TestCase):
 
@@ -33,3 +34,22 @@ class TestTreeDataStructures(unittest.TestCase):
         # test that values are not present in BST
         for val in seq:
             self.assertEqual(BST.find(val), None)
+
+    def test_binary_heap(self):
+        BH = BinaryHeap()
+
+        BH.insert(9)
+        BH.insert(6)
+        BH.insert(5)
+        BH.insert(3)
+        BH.insert(2)
+        self.assertEqual([2, 3, 6, 9, 5], BH.heap)
+        self.assertEqual(5, BH.size)
+
+        BH.heapify([21, 19, 18, 17, 11, 14, 9])
+        self.assertEqual([9, 11, 14, 17, 19, 21, 18], BH.heap)
+        self.assertEqual(7, BH.size)
+
+        BH.delete_min()
+        self.assertEqual([11, 17, 14, 18, 19, 21], BH.heap)
+        self.assertEqual(6, BH.size)
